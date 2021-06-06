d9a23d500c54a6ee136bbd5adc3ea6f6b82f9452
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2021-06-06 18:03:37 +0900

    자바 객체지향 강의 33강 - 새로운 객체로 바꾸기(문자열을 읽어서 객체로 만들기)

--- OOPJavaPrj/src/part3/ex6/인터페이스/Program.java
+++ OOPJavaPrj/src/part3/ex6/인터페이스/Program.java
@@ -1,11 +1,23 @@
 package part3.ex6.인터페이스;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        FileInputStream fis = new FileInputStream("src/part3/ex6/인터페이스/setting.txt");
+        Scanner scan = new Scanner(fis);
+        String className = scan.nextLine();
+        scan.close();
+        fis.close();
+
+        Class clazz = Class.forName(className);
+
         A a = new A();
-        B b = new B();
-        a.setX(b);
+        X x = (X) clazz.newInstance();
+        a.setX(x);
         a.print();
     }
 
