a45a93cebd06ad2fd813349a3edc1417b3925b7f
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2021-06-06 17:35:48 +0900

    자바 객체지향 강의 32강 - 객체 결합을 위한 인터페이스 구현하기

---
+++ OOPJavaPrj/src/part3/ex6/인터페이스/Program.java
@@ -0,0 +1,12 @@
+package part3.ex6.인터페이스;
+
+public class Program {
+
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.setX(b);
+        a.print();
+    }
+
+}
