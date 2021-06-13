395e23fa4df74a31f8f983b9ed73babb507046fc
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2021-06-13 19:32:01 +0900

    서블릿/JSP 강의 19 - 한글 입력 문제

--- JSPPrj/src/com/newlecture/web/NoticeReg.java
+++ JSPPrj/src/com/newlecture/web/NoticeReg.java
@@ -15,6 +15,7 @@ public class NoticeReg extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
 
         PrintWriter out = response.getWriter();
 
